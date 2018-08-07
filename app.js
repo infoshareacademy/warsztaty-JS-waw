033f54ccdf3dc132550a979d5fdaaaeee5947eed
Krzyku <jasinski.it@gmail.com>
2018-08-07 21:01:09 +0200

    add js logic

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var numberOfSymbols = 8;
 var symbols = [];
+var cards = []
 
 function prepareSymbols() {
     for (var i = 0 ; i < numberOfSymbols ; i++) {
@@ -31,13 +32,58 @@ function renderCards() {
 
         copy.removeAttribute('id')
         copy.querySelector('.back').innerText = symbol
+        copy.dataset.symbol = symbol
 
+        cards.push(copy)
         container.appendChild(copy)
+    })
+}
+
+function addClickListeners() {
+    var selected = []
+    var matched = []
+    
+    cards.forEach(card => {
+        card.addEventListener('click', function() {
+            // Czy kliknieta 2 x ta sama karta
+            // lub kliknieta karta ktora wygrała
+            if (selected.includes(card) || matched.includes(card)) {
+                return
+            }
+
+            // Czy zaznaczone dwie karty
+            if (selected.length === 2) {
+                selected[0].classList.remove('selected')
+                selected[1].classList.remove('selected')
+
+                selected = []
+            }
+            
+            // Zaznacz karte
+            card.classList.add('selected')
+            selected.push(card)
+
+            // Czy wygrana
+            if (selected.length === 2 && selected[0].dataset.symbol === selected[1].dataset.symbol) {
+                selected[0].classList.remove('selected')
+                selected[0].classList.add('matched')
+
+                selected[1].classList.remove('selected')
+                selected[1].classList.add('matched')
+
+                matched.push(selected[0], selected[1])
+            }
+
+            if (matched.length === cards.length) {
+                alert('Hurrraaaaaa!!!!!')
+            }
+        })
     })
 }
 
 prepareSymbols()
 shuffleSymbols()
 renderCards()
+addClickListeners()
 
 console.log(symbols)
